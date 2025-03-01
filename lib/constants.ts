d75beae605336a748dc3d50fbc743b3213d6537c
anish-singh-rawat <www.computernetwork@gmail.com>
2025-03-01 09:51:43 +0530

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const SENDER_NAME = process.env.SENDER_NAME || 'Anish'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'anishsinghrawat5@gmail.com'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'IndianBaazaar'
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']

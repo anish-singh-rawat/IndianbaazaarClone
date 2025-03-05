ec6c1eada7ba3e12b68b8bc1469cb99fa9c3b857
anishsinghQB <anish.singh@quadbtech.com>
2025-03-05 12:39:51 +0530

    fix backend correct code for mongodb with correct TS code

--- app/api/messages/mongoDB.ts
+++ app/api/messages/mongoDB.ts
@@ -8,16 +8,19 @@ if (!uri) {
 }
 
 const options: MongoClientOptions = {};
-
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+const globalWithMongo = globalThis as unknown as {
+  _mongoClientPromise?: Promise<MongoClient>;
+};
+
 if (process.env.NODE_ENV === 'development') {
-  if (!(global as any)._mongoClientPromise) {
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();

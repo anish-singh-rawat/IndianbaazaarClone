0e32aec99441cfc1ea081a9f05bc974087ae02e6
anishsinghQB <anish.singh@quadbtech.com>
2025-03-05 13:15:04 +0530

    fix meta data and add head file in app for create app SEO friendly

---
+++ app/[locale]/head.jsx
@@ -0,0 +1,23 @@
+export default function Head() {
+    return (
+      <>
+        <title>
+          Indian Baazaar: Ultimate Online Destination for Fashion, Beauty, and Lifestyle
+        </title>
+        <meta
+          name="description"
+          content="Discover top trends in fashion and lifestyle at Indian Baazaar – your ultimate destination for cosmetics, shoes, bags, toys, and clothing."
+        />
+        <meta name="keywords" content="Indian Baazaar, e-commerce, fashion, etc." />
+        <meta property="og:title" content="Indian Baazaar: Ultimate Online Destination" />
+        <meta property="og:url" content="https://www.indianbaazaar.com/" />
+        <meta
+          property="og:description"
+          content="Explore a wide range of cosmetics, shoes, bags, toys, and clothing for women, men, and kids."
+        />
+        <meta property="og:image" content="https://www.indianbaazaar.com/icons/logo.svg" />
+        <meta property="og:type" content="website" />
+      </>
+    );
+  }
+  
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import { notFound } from 'next/navigation'
 import { getSetting } from '@/lib/actions/setting.actions'
 import { cookies } from 'next/headers'
 import ChatBot from '@/components/ChatBot/ChatBot'
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -19,6 +20,72 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 })
+export const metadata = {
+  title: 'Indian Baazaar: ultimate online destination for fashion, beauty, and lifestyle products',
+
+  description:
+    'Discover top trends in fashion and lifestyle products at Indian Baazaar, also known as Indian Bazaar. Shop our exclusive collections of cosmetics, shoes, bags, toys, and clothing.',
+
+  keywords: [
+    'Indian Baazaar',
+    'Indian Bazaar',
+    'IndianBaazaar',
+    'IndianBazaar',
+    'Indianbazaar',
+    'indianBazaar',
+    'indianBazaar',
+    'Indian Baazaar',
+    'Indian Bazaar',
+    'Indian bazaar',
+    'indian Bazaar',
+    'indian Baazaar',
+    'e-commerce',
+    'Online shopping',
+    'internet shopping',
+    'web shopping',
+    'e-tailing',
+    'online retailing',
+    'web-based commerce',
+    'electronic commerce',
+    'digital commerce',
+    'fashion',
+    'beauty',
+    'lifestyle',
+    'cosmetics',
+    'shoes',
+    'bags',
+    'toys',
+    'clothing',
+    'online shopping',
+    'e-commerce',
+    'style',
+    'elegance',
+    'play',
+  ],
+  viewport: 'width=device-width, initial-scale=1.0',
+  openGraph: {
+    title: 'Indian Baazaar: ultimate online destination for fashion, beauty, and lifestyle products',
+    url: 'https://www.indianbaazaar.com/',
+    description:
+      'Indian Baazaar || Next Ecommerce is a sample Ecommerce website built with Next.js, Tailwind CSS, and MongoDB. Indian Baazaar, also known as Indian Bazaar, is your ultimate online destination for fashion, beauty, and lifestyle products. Explore a wide range of cosmetics, shoes, bags, toys, and clothing for women, men, and kids. (Indian Bazaar) | Style, Elegance, and Play - All in One Place!',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.indianbaazaar.com/icons/logo.svg',
+        secureUrl: 'https://www.indianbaazaar.com/icons/logo.svg',
+        width: 600,
+        height: 315,
+      },
+    ],
+  },
+  icons: {
+    icon: 'https://www.indianbaazaar.com/icons/logo.svg',
+    shortcut: 'https://www.indianbaazaar.com/icons/logo.svg',
+    apple: 'https://www.indianbaazaar.com/icons/logo.svg',
+  },
+};
+
+
 
 export async function generateMetadata() {
   const {
@@ -46,8 +113,6 @@ export default async function AppLayout({
   const currency = currencyCookie ? currencyCookie.value : 'USD'
 
   const { locale } = await params
-  // Ensure that the incoming `locale` is valid
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!routing.locales.includes(locale as any)) {
     notFound()
   }
@@ -72,7 +137,18 @@ export default async function AppLayout({
             <ChatBot />
           </div>
         </div>
-
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-13VDW9PYVP"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-13VDW9PYVP');
+          `}
+        </Script>
       </body>
     </html>
   )
